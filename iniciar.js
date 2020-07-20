2a9564095584f7d3835ff51f18de086e2b2a58d5
Benjamin Chimoy <benjamin@chimoy.com>
2020-07-20 09:21:45 +0200

    Done

--- iniciar.js
+++
@@ -1,9 +0,0 @@
-let grupoTarjetas = ["😀", "🦄", "L", "I"];
-
-iniciarCronometro();
-
-let totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
-reparteTarjetas();
-document.querySelectorAll(".tarjeta").forEach((element) => {
-  element.addEventListener("click", descubrir);
-});
